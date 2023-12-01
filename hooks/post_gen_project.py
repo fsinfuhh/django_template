4a30e6d311d904ddf371f777824d44f0fbe8aa48
ftsell <dev@ftsell.de>
2023-12-01 15:28:25 +0100

    add frontend part to template

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 import subprocess
 
-if __name__ == "__main__":
-    subprocess.check_call("git init".split(" "))
-    subprocess.check_call("pre-commit install".split(" "))
-    subprocess.check_call("pipenv lock".split(" "))
+
+def git_commit(message: str):
     subprocess.check_call("git add --all".split(" "))
     subprocess.call("pre-commit run --all-files".split(" "))
     subprocess.check_call("git add --all".split(" "))
-    subprocess.check_call(
-        ["git", "commit", "--message=add project setup from mafiasi template"]
-    )
+    subprocess.check_call(["git", "commit", f"--message={message}"])
+
+
+if __name__ == "__main__":
+    # add generated files
+    subprocess.check_call("pre-commit install".split(" "))
+    git_commit("add project setup from mafiasi template")
+
+    # lock & install dependencies
+    subprocess.check_call("pipenv lock".split(" "))
+    subprocess.check_call("pipenv sync".split(" "))
+    subprocess.check_call("pnpm install".split(" "), cwd="./src/{{ cookiecutter.project_slug }}_gui")
+    git_commit("lock dependencies")
--- {{ cookiecutter.project_slug }}/src/{{ cookiecutter.project_slug }}_gui/nuxt.config.ts
+++ {{ cookiecutter.project_slug }}/src/{{ cookiecutter.project_slug }}_gui/nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  extends: ["nuxt_layer_mafiasi"],
+  runtimeConfig: {
+    public: {
+      apiBase: "",
+    }
+  },
 })
